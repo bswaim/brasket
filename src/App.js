2fac5500ecc0193892e468bdb74e486b7a63fea1
bswaim <brandy.brasket@gmail.com>
2024-02-20 01:04:35 -0600

    init gh-pages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import LandingPage from "./components/environment/LandingPage";
+import './index.css';
+import './css/atom/toggle.css'
+import {useState} from "react";
+// import {Provider} from "react-redux";
+// import store from "./redux/store";
+
+function App() {
+
+  const [themeSelection, setThemeSelection] = useState(true);
+  const theme = themeSelection ? 'dark-theme' : 'light-theme';
+
+  return (
+      <div id="page" className={theme}>
+          <LandingPage
+              themeSelection={themeSelection}
+              setThemeSelection={e => setThemeSelection(e)}
+           />
+      </div>
+  );
+}
+
+export default App;
